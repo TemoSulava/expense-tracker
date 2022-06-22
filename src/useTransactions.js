a5ab8a3db20c5686c58dd2a7629d64a273caf6a8
TemoSulava <sulava.temo@yahoo.com>
2022-06-22 19:27:09 +0400

    implemented custom transaction hook

---
+++ src/useTransactions.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react'
+import { ExpenseTrackerContext } from './context/context'
+
+import { incomeCategories, expenseCategories, resetCategories } from './constants/categories'
+
+const useTransactions = (title) => {
+  resetCategories()
+  const { transactions } = useContext(ExpenseTrackerContext)
+  const transactionsOfEachType = transactions.filter((tr) => tr.type === title)
+
+  const total = transactionsOfEachType.reduce((acc, cur) => (acc += cur.amount), 0)
+  const categories = title === 'Income' ? incomeCategories : expenseCategories
+
+  console.log({ transactionsOfEachType, total, categories })
+
+  transactionsOfEachType.forEach((tr) => {
+    const category = categories.find((cat) => cat.type === tr.category)
+
+    if (category) category.amount += tr.amount
+  })
+
+  const filteredOutCategories = categories.filter((cat) => cat.amount > 0)
+
+  const chartData = {
+    datasets: [
+      {
+        data: filteredOutCategories.map((cat) => cat.amount),
+        backgroundColor: filteredOutCategories.map((cat) => cat.color)
+      }
+    ],
+    labels: filteredOutCategories.map((cat) => cat.type)
+  }
+
+  return {filteredOutCategories, total, chartData}
+}
+
+export default useTransactions
