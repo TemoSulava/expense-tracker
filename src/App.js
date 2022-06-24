c16bd3a055c908ed927f9d25d1a64220e0c40d27
TemoSulava <sulava.temo@yahoo.com>
2022-06-24 17:52:48 +0400

    made mobile responnsive, moved tracker to the top on mobile devices

--- src/App.js
+++ src/App.js
@@ -13,14 +13,23 @@ const App = () => {
 
   return (
     <div>
-      <Grid className={classes.grid} container spacing={0} aligntItems='center' justifyContent='center' style={{ height: '100vh' }}>
-        <Grid item xs={12} sm={4}>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        aligntItems='center'
+        justifyContent='center'
+        style={{ height: '100vh' }}>
+        <Grid className={classes.mobile} item xs={12} sm={4}>
           <Details title='Income' />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid className={classes.desktop} item xs={12} sm={4}>
+          <Details title='Income' />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title='Expense' />
         </Grid>
       </Grid>
